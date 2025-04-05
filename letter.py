12935ba0483fb27e4bdf8cf3dd916d589b10c606
Samuel Lawal <samuellawal667@gmail.com>
2025-04-05 14:21:04 -0800

    Create letter.py

---
+++ letter.py
@@ -0,0 +1,14 @@
+## Snail Mail 💌
+# Codédex
+print('|                                                         April 2025  |')
+print('|                                                          ojo,lagos  |')
+print('|  Dear Self,                                                         |')
+print('|                                                                     |')
+print('|      Build the learn to code platform that you always dreamed of.   |')
+print('|      Give more than you take.                                       |')
+print('|      practice makes perfect.                                                |')
+print('|      Do not be to quick to give up.                                 |')
+print('|      And always get back up.                                        |')
+print('|                                                                     |')
+print('|                                              Samuellawal🤠            |')
+print('|                                                                     |')
